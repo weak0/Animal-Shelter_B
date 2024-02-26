1084e1d4d5ba5af662637501c7381b086857d58e
weak0 <maciej.gorzela89@gmail.com>
2024-02-26 20:34:23 +0100

    Updated to  8.0.1 .NET

---
+++ Animal Shelter/Entities/AnimalShelterContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Animal_Shelter.Entities;
+
+public class AnimalShelterContext : DbContext
+{
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options) : base(options)
+    {
+    }
+    
+    
+}
--- Animal Shelter/Program.cs
+++ Animal Shelter/Program.cs
@@ -1,15 +1,17 @@
+using Animal_Shelter.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AnimalShelterContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
