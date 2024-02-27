46da723294268671339d0f2111086b5b4d3b3620
weak0 <maciej.gorzela89@gmail.com>
2024-02-27 20:00:50 +0100

    Test commit

---
+++ Animal Shelter/Entities/Shelter.cs
@@ -0,0 +1,12 @@
+﻿namespace Animal_Shelter.Entities;
+
+public class Shelter
+{
+    public int ShelterId { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set;}
+    public string City { get; set; }
+    public  string Street { get; set; }
+    public string ZipCode { get; set; }
+}
