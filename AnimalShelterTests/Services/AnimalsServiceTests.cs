4d2a5b0c643a4453c585862834a0bc83e8364838
Dorota <dorota.szota@edu.uekat.pl>
2024-04-04 10:03:31 +0200

    AnimalsServiceTests: 1st test submitted successfully

---
+++ AnimalShelterTests/Services/AnimalsServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Animal_Shelter.Entities;
+using Animal_Shelter.Mappers;
+using Animal_Shelter.Models;
+using Animal_Shelter.Services;
+using AnimalShelterTests.Fixtures;
+using AutoMapper;
+
+
+namespace AnimalShelterTests.Services;
+
+public class AnimalsServiceTests: IClassFixture<AnimalShelterDbContextFixture>
+{
+    private readonly AnimalShelterDbContextFixture _fixture;
+    private readonly IConfigurationService _configurationService;
+    private readonly AnimalsService _animalsService;
+
+    public AnimalsServiceTests(AnimalShelterDbContextFixture fixture)
+    {
+        _fixture = fixture;
+        var mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<AnimalShelterMappingProfile>()));
+        var shelterService = new ShelterService(fixture.Db);
+        var configurationService = new ConfigurationService(fixture.Db, mapper, shelterService);
+        _animalsService = new AnimalsService(_fixture.Db, mapper, shelterService);
+    }
+    
+    [Fact]
+    public async Task AddAnimal_ShouldAddAnimalAndOk_WhenDataIsValid()
+    {
+        //Arrange
+        var dto = new AddAnimalDto()
+        {
+            AnimalName = "test1",
+            Type = AnimalType.Cat,
+            Size = AnimalSize.Small,
+            ShelterId = 1,
+            
+        };
+        //Act
+        var serviceResponse = await _animalsService.AddAnimal(dto);
+        //Assert
+        Assert.NotNull(serviceResponse);
+        Assert.Equal(dto.AnimalName, serviceResponse.AnimalName);
+        Assert.Equal(dto.Type, serviceResponse.Type);
+        Assert.Equal(dto.Size, serviceResponse.Size);
+        Assert.Equal(dto.ShelterId, serviceResponse.ShelterId);
+    }
+    
+}
