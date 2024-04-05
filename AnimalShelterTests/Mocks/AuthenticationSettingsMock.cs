f78b297d973fe286093ae9714dd1a5d84706508c
weak0 <maciej.gorzela89@gmail.com>
2024-04-05 20:10:40 +0200

    2

--- AnimalShelterTests/Mocks/AuthenticationSettingsMock.cs
+++ AnimalShelterTests/Mocks/AuthenticationSettingsMock.cs
@@ -4,7 +4,7 @@ namespace AnimalShelterTests.Mocks;
 
 public class AuthenticationSettingsMock : IAuthenticationSettings
 {
-    public string JwtKey { get; set; } = "this is a test key with a minimum length of 16 characters";
+    public string JwtKey { get; set; } = "JanPawelGralWKartyIMiałŁepObdarty";
     public int ExpiresDate { get; set; } = 1;
     public string JwtIssuer { get; set; } = "test";
 }
