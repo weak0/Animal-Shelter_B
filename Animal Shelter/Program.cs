e4656cb5eb1a34910ab46d50f36e4ff29ae844a2
weak0 <maciej.gorzela89@gmail.com>
2024-04-06 12:28:36 +0200

    10

--- Animal Shelter/Program.cs
+++ Animal Shelter/Program.cs
@@ -23,9 +23,9 @@ builder.Services.AddScoped<IPasswordHasher<Shelter>, PasswordHasher<Shelter>>();
 var authSettings = new AuthenticationSettings();
 if (Environment.GetEnvironmentVariable("CI") == "true")
 {
-    // authSettings.ExpiresDate = int.Parse(Environment.GetEnvironmentVariable("AUTH_JWT_DATE"));
-    authSettings.JwtIssuer = Environment.GetEnvironmentVariable("AUTH_JWT_ISSUER");
-    authSettings.JwtKey = Environment.GetEnvironmentVariable("AUTH_JWT_KEY");
+    authSettings.ExpiresDate = int.Parse(Environment.GetEnvironmentVariable("AUTH_JWT_DATE")?? throw new InvalidOperationException()) ;
+    authSettings.JwtIssuer = Environment.GetEnvironmentVariable("AUTH_JWT_ISSUER") ?? throw new InvalidOperationException();
+    authSettings.JwtKey = Environment.GetEnvironmentVariable("AUTH_JWT_KEY") ?? throw new InvalidOperationException();
 }
 else builder.Configuration.GetSection("Auth").Bind(authSettings);
 
